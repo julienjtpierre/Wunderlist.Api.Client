6d978eb96e7f78e288d8b659c86abe76d2e70d26
Julien Pierre <jupierr@microsoft.com>
2016-07-11 12:38:42 +0800

    changes

---
+++ Wunderlist.Api.Client/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wunderlist.Api.Client.Extensions
+{
+    public static class Extensions
+    {
+        public static UpdateTaskRequest ToUpdateTaskRequest(this Task value)
+        {
+            UpdateTaskRequest r = new UpdateTaskRequest
+            {
+                AssigneeId = value.AssigneeId,
+                Completed = value.Completed,
+                DueDate = value.DueDate,
+                Starred = value.Starred,
+                Title = value.Title,
+                Revision = value.Revision
+            };
+
+            return r;
+        }
+    }
+}
